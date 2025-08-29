b59e4ace29f785ef066a7716331e59d1236a0bc4
mongoose_in_town <khaledbinnasir1714412140@gmail.com>
2025-08-29 16:16:05 +0600

    Implemented the clear function also solved the structure got destructuring problem due to over load of call history

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 const hamburger_bars_icon = document.getElementById('hamburgerBarsIcon');
 // console.log(hamburger_bars_icon);
-hamburger_bars_icon.addEventListener('click',function(){
+hamburger_bars_icon.addEventListener('click', function () {
     const hamburger_section = document.getElementById('hamburgerSection');
     hamburger_section.style.right = '0';
     hamburger_section.style.display = 'block';
 })
 
 const hamburger_cross_mark = document.getElementById('hamburgerCrossMark');
-hamburger_cross_mark.addEventListener('click',function(){
+hamburger_cross_mark.addEventListener('click', function () {
     const hamburger_section = document.getElementById('hamburgerSection');
     hamburger_section.style.right = '100%';
     hamburger_section.style.display = 'hidden';
@@ -16,29 +16,43 @@ hamburger_cross_mark.addEventListener('click',function(){
 // Calling function works from here
 const callBtn = document.querySelectorAll('.call-btn');
 const historyList = document.querySelector('ul');
-for (let i=0;i<callBtn.length;i++)
-{
-    callBtn[i].addEventListener('click',function(){
+for (let i = 0; i < callBtn.length; i++) {
+    callBtn[i].addEventListener('click', function () {
         const name = this.getAttribute('data-name');
         const number = this.getAttribute('data-number');
         const time = new Date().toLocaleTimeString();
-        const li = document.createElement('li');
-        li.className = 'p-[16px] flex justify-between items-center bg-[#FAFAFA] rounded-[8px]';
-        const leftDiv = document.createElement('div');
-        const nameP = document.createElement('p');
-        nameP.className = 'h font-semibold text-[16px] text-[#111111]';
-        nameP.innerText = name;
-        const numberP = document.createElement("p");
-        numberP.className = "r text-[14px] text-[#5C5C5C]";
-        numberP.innerText = number;
-        leftDiv.appendChild(nameP);
-        leftDiv.appendChild(numberP);
-        const timeSpan = document.createElement("span");
-        timeSpan.className = "r text-[14px] text-[#5C5C5C]";
-        timeSpan.innerText = time;
-        li.appendChild(leftDiv);
-        li.appendChild(timeSpan);
-        historyList.insertBefore(li, historyList.firstChild);
+
+        const choice = confirm("Do you want to call " + name + " (" + number + ")?");
+
+        if (choice)
+        {
+            const li = document.createElement('li');
+            li.className = 'p-[16px] flex justify-between items-center bg-[#FAFAFA] rounded-[8px]';
+            const leftDiv = document.createElement('div');
+            const nameP = document.createElement('p');
+            nameP.className = 'h font-semibold text-[16px] text-[#111111]';
+            nameP.innerText = name;
+            const numberP = document.createElement("p");
+            numberP.className = "r text-[14px] text-[#5C5C5C]";
+            numberP.innerText = number;
+            leftDiv.appendChild(nameP);
+            leftDiv.appendChild(numberP);
+            const timeSpan = document.createElement("span");
+            timeSpan.className = "r text-[14px] text-[#5C5C5C]";
+            timeSpan.innerText = time;
+            li.appendChild(leftDiv);
+            li.appendChild(timeSpan);
+            historyList.insertBefore(li, historyList.firstChild);
+        }
+        else
+        {
+            alert("Call cancelled");
+        }
     })
 }
 // Calling function ends from here
+
+const clearBtn = document.querySelector('.clear-btn');
+clearBtn.addEventListener('click',function(){
+    historyList.innerHTML = '';
+})
